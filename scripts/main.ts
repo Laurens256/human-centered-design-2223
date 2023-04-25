554f573ee700b9e3db8f3eea302ccfd99b54d171
Laurens256 <duinlaurens2@gmail.com>
2023-04-25 12:39:16 +0200

    add hints

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,9 +1,19 @@
 import { upperClothes, lowerClothes } from './clothes';
 import { initClothes } from './buildHTML';
+import { srSpeak } from './screenReader';
 
 const init = () => {
 	initClothes('upper', upperClothes);
 	initClothes('lower', lowerClothes);
+	document.addEventListener('keydown', (e) => {
+		if (e.ctrlKey) {
+			e.preventDefault();
+			srSpeak(
+				'druk op q om stijlen van geselecteerde kleding te horen, a om de eigenschappen te horen',
+				'assertive'
+			);
+		}
+	});
 };
 
 init();
--- scripts/screenReader.ts
+++ scripts/screenReader.ts
@@ -1,9 +1,7 @@
 const speakDiv = document.querySelector('div.speak') as HTMLDivElement;
 
 const srSpeak = (text: string, priority: 'polite' | 'assertive' = 'polite') => {
-	// const speakDiv = document.createElement("div");
 	speakDiv.setAttribute('aria-live', priority);
-
 	speakDiv.textContent = text;
 };
 
