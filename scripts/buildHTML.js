6f7b7ede983e4e86decb3407c78e96bf4417cd6d
Laurens256 <duinlaurens2@gmail.com>
2023-05-10 16:41:59 +0200

    new stuff B)

--- scripts/buildHTML.js
+++ scripts/buildHTML.js
@@ -1,108 +1,35 @@
-import { readClothingInfo } from './screenReader.js';
-import { upperClothes, lowerClothes } from './clothes.js';
+import { upperClothes, lowerClothes } from './clothes';
 
-const initClothes = (target, clothesArr) => {
-	const clothesFieldset = document.querySelector(
-		`fieldset.${target}-clothes`
-	);
+const upperClothingContainer = document.querySelector('.upper-clothing-container');
+const lowerClothingContainer = document.querySelector('.lower-clothing-container');
+// load clothing html
+const loadHTML = () => {
+	const upperClothesHTML = upperClothes
+		.map((item) => generateClothingHTML(item, 'bovenkleding'))
+		.join('');
 
-	if (!clothesFieldset) return;
-	const fragment = document.createDocumentFragment();
+	const lowerClothesHTML = lowerClothes
+		.map((item) => generateClothingHTML(item, 'onderkleding'))
+		.join('');
 
-	clothesArr
-		.sort((a, b) => {
-			const nameA = a.name.toLowerCase();
-			const nameB = b.name.toLowerCase();
-
-			if (nameA < nameB) return -1;
-			if (nameA > nameB) return 1;
-
-			return 0;
-		})
-		.forEach((clothes) => {
-			const { name, styles, attributes } = clothes;
-
-			const label = createElement('label', { textContent: name, className: 'clothes' });
-
-			const input = createElement(
-				'input',
-				{
-					type: 'radio',
-					name: `${target}`,
-					value: name
-				},
-				{
-					'data-styles': styles?.join(';'),
-					'data-attributes': attributes?.join(';'),
-					'data-name-nl': target === 'upper' ? 'bovenkleding' : 'onderkleding'
-				}
-			);
-
-			input.addEventListener('keydown', readClothingInfo);
-			input.addEventListener('change', generateSummary);
-
-			label.appendChild(input);
-			fragment.appendChild(label);
-		});
-
-	clothesFieldset.appendChild(fragment);
+	upperClothingContainer.insertAdjacentHTML('beforeend', upperClothesHTML);
+	lowerClothingContainer.insertAdjacentHTML('beforeend', lowerClothesHTML);
 };
 
-const generateSummary = (e) => {
-	const target = e.target;
+// build clothing html
+const generateClothingHTML = (item, queryValue) => {
+	const { name, styles, attributes, colors } = item;
+	const slug = name.toLowerCase().replace(' ', '-');
+	const stylesString = styles.join(';');
+	const attributesString = attributes.join(';');
+	const colorsString = colors.join(', ');
 
-	const { name, value, dataset } = target;
-
-	if (name === 'upper') {
-		selectedUpper = upperClothes.find((clothes) => clothes.name === value) || null;
-	} else {
-		selectedLower = lowerClothes.find((clothes) => clothes.name === value) || null;
-	}
-
-	const li = document.querySelector(`li.selected-${name}`);
-	if (li) {
-		if (target.checked) {
-			li.textContent = `${dataset.nameNl}: ${value}`;
-		} else {
-			li.textContent = `${dataset.nameNl}: niet gekozen`;
-		}
-	}
-	checkIfMatches();
+	return `
+	  <a href="#${slug}" class="clothing" data-query="${queryValue}" data-query-value="${slug}"
+		data-styles="${stylesString}" data-attributes="${attributesString}">
+		${name}: ${colorsString}
+	  </a>
+	`;
 };
 
-let selectedUpper = null;
-let selectedLower = null;
-const matchText = document.querySelector('p.matches');
-const checkIfMatches = () => {
-	let output = '';
-	if (!selectedUpper || !selectedLower) {
-		output = 'Selecteer eerst boven- en onderkleding';
-	} else {
-		const { styles: upperStyles } = selectedUpper;
-		const { styles: lowerStyles } = selectedLower;
-
-		const matchingStyles = upperStyles?.filter((attr) => lowerStyles?.includes(attr));
-
-		if (matchingStyles?.length) {
-			output = `Deze kledingstukken matchen met de volgende eigenschappen: ${matchingStyles.join(
-				', '
-			)}`;
-		} else {
-			output = 'De stijl van deze kledingstukken matched niet';
-		}
-	}
-	matchText.textContent = output;
-};
-
-const createElement = (tagName, props, dataset) => {
-	const element = document.createElement(tagName);
-	Object.assign(element, props);
-	if (dataset) {
-		Object.keys(dataset).forEach((key) => {
-			element.setAttribute(key, dataset[key]);
-		});
-	}
-	return element;
-};
-
-export { initClothes };
+export { loadHTML };
--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,71 @@
-import { upperClothes, lowerClothes } from './clothes.js';
-import { initClothes } from './buildHTML.js';
-import { srSpeak } from './screenReader.js';
+import { loadHTML } from './buildHTML';
 
+let queryElements;
 const init = () => {
-	initClothes('upper', upperClothes);
-	initClothes('lower', lowerClothes);
-	document.addEventListener('keydown', (e) => {
-		if (e.key === 'w') {
-			e.preventDefault();
-			srSpeak(
-				'druk op q om stijlen van geselecteerde kleding te horen, a om de eigenschappen te horen',
-				'assertive'
-			);
-		}
+	loadHTML();
+
+	queryElements = document.querySelectorAll('a[data-query]');
+	queryElements.forEach((element) => {
+		element.addEventListener('click', changeQuery);
 	});
 
-	const filterCheckboxes = document.querySelectorAll(
-		'input[type="checkbox"][name="stijl_filter"]'
-	);
-
-	filterCheckboxes.forEach((checkbox) => {
-		checkbox.addEventListener('change', filterClothes);
+	window.addEventListener('load', () => {
+		clearQuery();
 	});
 };
 
-let selectedFilters = new Set();
-const filterClothes = (e) => {
-	const target = e.target;
-	const { value } = target;
+// change url query on link click
+const changeQuery = (e) => {
+	e.preventDefault();
+	const currentTarget = e.currentTarget;
+	const { query, queryValue } = currentTarget.dataset;
 
-	target.checked ? selectedFilters.add(value) : selectedFilters.delete(value);
-	const allClothes = document.querySelectorAll('label.clothes');
+	const urlParams = new URLSearchParams(window.location.search);
+	urlParams.set(query, queryValue);
 
-	allClothes.forEach((clothes) => {
-		// check if any of the dataset styles, are in the selectedFilters set
-		const input = clothes.querySelector('input');
-		const { dataset } = input;
+	window.history.replaceState(null, null, `${window.location.pathname}?${urlParams}`);
 
-		if (dataset.styles?.split(';').some((style) => selectedFilters.has(style)) || selectedFilters.size === 0) {
-			clothes.classList.remove('hidden');
-		} else {
-			clothes.classList.add('hidden');
-			input.checked = false;
-			input.dispatchEvent(new Event('change'));
+	if (query === 'stijl') {
+		filterClothes(queryValue);
+	}
+
+	srSpeak(`geselecteerd: ${currentTarget.textContent}`, 'assertive');
+	jumpToNextCategory(currentTarget);
+};
+
+const clearQuery = () => {
+	window.history.replaceState(null, null, window.location.pathname);
+};
+
+// jump to next category after link is clicked
+const jumpToNextCategory = (current) => {
+	const nextParent = current.closest('.category').nextElementSibling;
+
+	if (nextParent?.classList.contains('category')) {
+		nextParent.querySelector('a[data-query]')?.focus();
+	}
+};
+
+// filter clothes based on style, hidden class gets hidden using css which also hides the element from screenreaders
+const filterClothes = (filter) => {
+	queryElements.forEach((element) => {
+		if (element.dataset.query !== 'stijl') {
+			const styles = element.dataset.styles?.split(';') || [];
+
+			if (styles.includes(filter) || filter === '') {
+				element.classList.remove('hidden');
+			} else {
+				element.classList.add('hidden');
+			}
 		}
 	});
+};
+
+const speakDiv = document.querySelector('div.speak');
+// make screenreader speak
+const srSpeak = (text, priority = 'polite') => {
+	speakDiv.setAttribute('aria-live', priority);
+	speakDiv.textContent = text;
 };
 
 init();
